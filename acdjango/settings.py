648962c98a41784f886dbdfb163ef8df80fd81ec
Robert Gray <rgray@ttsa.net>
2022-05-12 23:26:13 -0700

    changed to postgres db in settings.py

--- acdjango/settings.py
+++ acdjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'acdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aquaculture',
+        'USER': 'aquaculture',
+        'PASSWORD': 'aquaculture',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -123,4 +127,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
